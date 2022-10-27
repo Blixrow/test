029daad8359fa021f67e802caa841fa834dc634c
Blixrow <75791514+Blixrow@users.noreply.github.com>
2022-10-27 11:21:58 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,8 @@
+import subprocess
+
+subprocess.run(["cd /"])
+subprocess.run(["cd ../home/tls-sec/Desktop/"])
+
+for x in range(300):
+    with open("file"+str(x), 'w') as f:
+      f.write("trolled")
