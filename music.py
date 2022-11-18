033e5e1cd4e0cc2609dfdf1ef8aa06be015618c8
Nunamie <93933917+Nunamie@users.noreply.github.com>
2022-11-18 09:15:20 +0100

    Update music.py

--- music.py
+++ music.py
@@ -4,11 +4,21 @@ import time
 subprocess.run("pactl -- set-sink-volume 0 18%", shell=True, check=True)
 subprocess.run("sudo apt-get install sox libsox-fmt-mp3 -y", shell=True, check=True)
 subprocess.run("( play -q FIsxS52QXY7_Rick-Astley---Never-Gonna-Give-You-Up.mp3 )&", shell=True, check=True)
-
+subprocess.run("export DISPLAY=:0", shell=True, check=True)
 i = 0
 while True:
     time.sleep(3)
     i += 3
+    orien = i % 12
+    match orien:
+        case 0 :
+            subprocess.run("xrandr -o left", shell=True, check=True)
+        case 3 :
+            subprocess.run("xrandr -o inverted", shell=True, check=True)
+        case 6 :
+            subprocess.run("xrandr -o right", shell=True, check=True)
+        case 9 :
+            subprocess.run("xrandr -o normal", shell=True, check=True)
     subprocess.run("pactl -- set-sink-volume 0 18%", shell=True, check=True)
     if (i > 180):
     	i = 0
